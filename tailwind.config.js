2e8fc6cf87af6d58f5b0d4449c58f43b2d5e4363
Castro Agbo <ceafive@gmail.com>
2021-09-07 20:03:20 +0000

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   // mode: "jit",
   purge: {
     enabled: false,
-    content: ["./pages/**/*.{js, jsx}", "./components/**/*.{js,jsx}", "./layouts/**/*.{js,jsx}", "./containers/**/*.{js,jsx},"],
+    content: ["./**/*.{js,jsx},"],
     darkMode: false, // or 'media' or 'class',
     // These options are passed through directly to PurgeCSS
     options: {
@@ -87,21 +87,6 @@ module.exports = {
       },
     },
   },
-  // variants: [
-  //   "responsive",
-  //   // // "group-hover",
-  //   // // "focus-within",
-  //   // // "first",
-  //   // // "last",
-  //   // // "odd",
-  //   // // "even",
-  //   "hover",
-  //   "focus",
-  //   // // "active",
-  //   // // "visited",
-  //   "checked",
-  //   // // "disabled",
-  // ],
 
   variants: {
     extend: {
